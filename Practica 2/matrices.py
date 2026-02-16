0a937d4bdb966bb8f228697d2e8d96a65d11aede
afearvel <cloudafe@gmail.com>
2026-02-16 13:43:28 -0600

    CREATE matrices.py

---
+++ Practica 2/matrices.py
@@ -0,0 +1,11 @@
+A = [[5,6,13],[3,10,1],[2,11,3]]
+B = [[1,2,17],[6,5,15],[3,11,12]]
+
+c = [[0,0,0],[0,0,0],[0,0,0]]
+
+for i in range(len(A)):
+    for j in range(len(B[0])):
+        for k in range(len(B)):
+            c[i][j] += A[i][k] * B[k][j]
+
+print(c)
