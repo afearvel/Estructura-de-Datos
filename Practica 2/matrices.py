133f628ea016f3865a23e2620e6369aad931b70a
afearvel <cloudafe@gmail.com>
2026-02-18 12:23:22 -0600

    CREATE matrices.py

--- Practica 2/matrices.py
+++ Practica 2/matrices.py
@@ -9,3 +9,10 @@ for i in range(len(A)):
             c[i][j] += A[i][k] * B[k][j]
 
 print(c)
+
+
+C = [[sum(A[i][k]*B[k][j] for k in range(len(B)))
+      for j in range(len(B[0]))]
+      for i in range(len(A))]
+
+print(C)
