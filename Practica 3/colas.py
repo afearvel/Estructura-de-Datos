735c30d77ecb4ee38f9340db60c277d25552be6f
afearvel <cloudafe@gmail.com>
2026-03-02 12:45:00 -0600

    CREATE colas.py

---
+++ Practica 3/colas.py
@@ -0,0 +1,30 @@
+def enque(lista, elemento):
+    lista.append(elemento)
+
+def deque(lista):
+    lista.pop(0)
+
+def peek(lista):
+    return lista[0]
+
+def is_empty(lista):
+    if lista == []:
+        return True
+    else:
+        return False
+    
+def size(lista):
+    return len(lista)
+
+lista = []
+
+print(is_empty(lista))
+enque(lista, 1)
+print(lista)
+print(is_empty(lista))
+deque(lista)
+print(is_empty(lista))
+enque(lista, 2)
+enque(lista, 3)
+print(peek(lista))
+print(size(lista))
